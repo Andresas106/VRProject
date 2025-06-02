7ef0b5defb5065a77a590c41c983a44e2c076e46
Andreu Verdés <averdes12@gmail.com>
2025-06-03 00:47:47 +0200

    objetos destruibles hechos

--- SetUp/Assets/SetUp/Scripts/Pared/ParedInteraccion.cs
+++ SetUp/Assets/SetUp/Scripts/Pared/ParedInteraccion.cs
@@ -7,11 +7,17 @@ public class ParedInteraccion : MonoBehaviour
 {
 
     [SerializeField] private GameObject objectDestroyed;
+    [SerializeField] private GameObject cadenas;
 
     public void ActivateTarget()
     {
-        GameObject obj = Instantiate(objectDestroyed, transform.position, Quaternion.identity);
+        GameObject obj = Instantiate(objectDestroyed, transform.position, transform.rotation);
         Destroy(this.gameObject);
         Destroy(obj, 3f);
+
+        if (objectDestroyed.name == "TotemRoto" )
+        {
+            if(cadenas != null )  Destroy(cadenas);
+        }
     }
 }
