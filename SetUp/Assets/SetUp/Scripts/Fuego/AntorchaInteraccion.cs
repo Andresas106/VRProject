25551266939f341b2f7da5d486890ee95e0cfffb
Andreu Verdés <averdes12@gmail.com>
2025-06-02 18:24:29 +0200

    script luz antorcha modificado

--- SetUp/Assets/SetUp/Scripts/Fuego/AntorchaInteraccion.cs
+++ SetUp/Assets/SetUp/Scripts/Fuego/AntorchaInteraccion.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class AntorchaInteraccion : MonoBehaviour
 {
     [SerializeField] private GameObject objectToActivate;
+    [SerializeField] private GameObject particlesToActivate;
 
     public void ActivateTarget()
     {
         if (objectToActivate != null)
         {
             objectToActivate.SetActive(true);
+        }
+
+        if(particlesToActivate != null)
+        {
+            particlesToActivate.SetActive(true);
         }
     }
 }
