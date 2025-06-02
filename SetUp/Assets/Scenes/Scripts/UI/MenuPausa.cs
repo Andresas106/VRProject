01bd67e0a33916733ef4bcde2b3306136daf1d2f
Andreu Verdés <averdes12@gmail.com>
2025-06-02 21:55:08 +0200

    menu pausa hecho

---
+++ SetUp/Assets/Scenes/Scripts/UI/MenuPausa.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+
+    private bool isPaused = false;
+    public GameObject locomotionSystem;
+    [SerializeField] private GameObject panelPausa;
+
+    public void pausar()
+    {
+        isPaused = true;
+        panelPausa.SetActive(true); // Muestra el menú de pausa
+        locomotionSystem.SetActive(false);
+    }
+
+    public void menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void resume()
+    {
+        isPaused = false;
+        panelPausa.SetActive(false); // Oculta el menú de pausa
+        locomotionSystem.SetActive(true);
+    }
+}
