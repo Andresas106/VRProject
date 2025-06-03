9da398341b03efc42e81bedae7ef7b1249b555ce
Ed3X <edgar.obrador@estudiantat.upc.edu>
2025-06-03 11:40:03 +0200

    cosas

--- SetUp/Assets/Scenes/Scripts/UI/MenuPausa.cs
+++ SetUp/Assets/Scenes/Scripts/UI/MenuPausa.cs
@@ -1,27 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
 
-public class MenuPausa : MonoBehaviour
-{
-    public GameObject locomotionSystem;
-    [SerializeField] private GameObject panelPausa;
-
-    public void pausar()
+    public class MenuPausa : MonoBehaviour
     {
-        panelPausa.SetActive(true); // Muestra el menú de pausa
-        locomotionSystem.SetActive(false);
-    }
+        public GameObject locomotionSystem;
+        [SerializeField] private GameObject panelPausa;
 
-    public void menu()
-    {
-        SceneManager.LoadScene(0);
-    }
+        public void pausar()
+        {
+            panelPausa.SetActive(true); // Muestra el menú de pausa
+            locomotionSystem.SetActive(false);
+        }
 
-    public void resume()
-    {
-        panelPausa.SetActive(false); // Oculta el menú de pausa
-        locomotionSystem.SetActive(true);
+        public void menu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void resume()
+        {
+            panelPausa.SetActive(false); // Oculta el menú de pausa
+            locomotionSystem.SetActive(true);
+        }
     }
-}
